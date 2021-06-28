ac8fad11619704b0c433bb0302fbd6a485449338
Andrew <jimboy911@gmail.com>
2021-06-27 20:15:56 -0400

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+#TicTacToe Game
+
+def gameBoard():
+    FirstRow    = "|1|2|3|"
+    SecondRow   = "|4|5|6|"
+    ThirdRow    = "|7|8|9|"
+    print("Welcome to TicTacToe!\n")
+    print(FirstRow)
+    print(SecondRow)
+    print(ThirdRow)
+
+gameBoard()
+
+#next: ask player X to place an X on the gameBoard
