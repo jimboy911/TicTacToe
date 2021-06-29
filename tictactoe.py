3fc69ed2d30428f0aa5923dfed6d60f5d07d3dfd
Andrew <jimboy911@gmail.com>
2021-06-28 20:52:54 -0400

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,42 @@
 #TicTacToe Game
 
-def gameBoard():
-    FirstRow    = "|1|2|3|"
-    SecondRow   = "|4|5|6|"
-    ThirdRow    = "|7|8|9|"
+def main():
+    theBoard = gameBoard()
     print("Welcome to TicTacToe!\n")
-    print(FirstRow)
-    print(SecondRow)
-    print(ThirdRow)
+    print(theBoard)
+    round1 = playerX(theBoard)
+    print(round1)
+    round2 = playerO(round1)
+    print(round2)
+    round3 = playerX(round2)
+    print(round3)
+    round4 = playerO(round3)
+    print(round4)
+    round5 = playerX(round4)
+    print(round5)
+    round6 = playerO(round5)
+    print(round6)
+    round7 = playerX(round6)
+    print(round7)
+    round8 = playerO(round7)
+    print(round8)
+    round9 = playerX(round8)
+    print(round9)
 
-gameBoard()
+#display gameBoard
+def gameBoard():
+    gameBoard = "|1|2|3|\n|4|5|6|\n|7|8|9|\n"
+    return gameBoard
 
-#next: ask player X to place an X on the gameBoard
+def playerX(theBoard):
+    X_Move = str(input("Player X Move: "))
+    X_Board = theBoard.replace(X_Move, "X")
+    return X_Board
+
+def playerO(theBoard):
+    O_Move = str(input("Player O Move: "))
+    O_Board = theBoard.replace(O_Move, "O")
+    return O_Board
+
+#runs the program
+main()
